b96a0f5835d200b24dfb7fa1bb636fce4b5f6473
Tian Yu <ty277@cornell.edu>
2019-05-26 17:02:18 -0400

    adding bar plots and line plots

--- eda.py
+++ eda.py
@@ -15,6 +15,10 @@ class EDA():
         self.printTb = printTb
         self.summary = None
         self.freq = None
+        # parameters for plotting
+        self.mycolors = ['royalblue', 'firebrick', 'forestgreen', 'orange', 'grey',
+                         'darkturquoise', 'palevioletred', 'olive', 'sandybrown', 'mediumpurple']
+        self.opacity = 0.75
 
     def __missing_cnt(self, series):
         """
@@ -181,7 +185,7 @@ class EDA():
                 print t
 
     ############################ Numeric Variables: Histograms ##############################
-    def num_histogram(self, data, percentile_range=None, bins=30, num_list=None):
+    def num_histogram(self, data, percentile_range=None, bins=30, width=10, height=6, num_list=None):
         """
         input:
             data(dataframe)
@@ -213,14 +217,96 @@ class EDA():
             else:
                 title_msg = ""
 
-            plt.figure(figsize=(10, 6))
-            plt.hist(var, bins=bins, normed=True, color="#6495ED", alpha=0.85)
+            plt.figure(figsize=(width, height))
+            plt.hist(var, bins=bins, normed=True, color=self.mycolors[0], alpha=self.opacity)
             plt.title(v + ": non missing values" + title_msg)
             plt.xlabel("Value")
             plt.ylabel("Density")
             plt.show()
 
+    def plot_bar_over_buket(self, plot_df, varname, bktname, title=None, xlabel=None, ylabel=None,
+                            cl=None, figwidth=14, figheight=6,
+                            bar_width=.25, xticksrotation=90):
+        '''
+        plot_df: data frame for plot
+        varname: column name of the variable to be ploted over different buckets
+        bktname: bucket column name
+        '''
+        n_groups = len(plot_df)
+        if cl is None:
+            cl = self.mycolors[0]
+        if title is None:
+            title = varname + ' by ' + bktname
+        if xlabel is None:
+            xlabel = bktname
+        if ylabel is None:
+            ylabel = varname
 
-    # TODO:  add bar and line chart to look at univariate relationship
+        # create plot
+        fig, ax = plt.subplots()
+        fig.set_figwidth(figwidth)
+        fig.set_figheight(figheight)
+        index = np.arange(n_groups)
+
+        plt.bar(index + bar_width, plot_df[varname], width=bar_width, label=varname, color=cl, alpha=self.opacity)
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        plt.title(title)
+        plt.xticks(index + bar_width, plot_df[bktname], rotation=xticksrotation)
+        plt.legend(loc=2)
+
+        plt.tight_layout()
+        plt.show()
+
+    def plot_bar_line_over_bkt(self, plot_df, barvar, linevar, bktvar,
+                               barvar2=None, linevar2=None,
+                               title=None, xlabel=None, ylabel1=None, ylabel2=None,
+                               width=14, height=6, bar_width=0.25):
+        '''
+        barvar: column name of the variable to be ploted in bars over different buckets
+        linevar: column name of the variable to be ploted as line over different buckets
+        bktname: bucket column name
+        '''
+        if title is None:
+            title = ', '.join([barvar, linevar]) + ' by ' + bktvar
+        if xlabel is None:
+            xlabel = bktvar
+        if ylabel1 is None:
+            ylabel1 = barvar
+        if ylabel2 is None:
+            ylabel2 = linevar
+        # bar chart
+        n_groups = len(plot_df)
+
+        # create plot
+        fig, ax = plt.subplots()
+        fig.set_figwidth(width)
+        fig.set_figheight(height)
+        index = np.arange(n_groups)
+        ax.bar(index - bar_width / 2., plot_df[barvar], bar_width,
+               alpha=self.opacity,
+               color=self.mycolors[0],
+               label=barvar)
+        if barvar2 is not None:
+            ax.bar(index + bar_width / 2., plot_df[barvar2], bar_width,
+                   alpha=self.opacity,
+                   color=self.mycolors[1],
+                   label=barvar2)
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel1)
+        plt.title(title)
+        plt.xticks(index + bar_width, plot_df[bktvar], rotation=90)
+        plt.legend()
+
+        ax2 = ax.twinx()
+        # if not comparing two pairs of values, use a different color for line, if yes, use the same color
+        cidx = 1 if (barvar2 is None) else 0
+        ax2.plot(index + bar_width / 2.0, plot_df[linevar], color=self.mycolors[cidx], linestyle='--')
+        if linevar2 is not None:
+            ax2.plot(index + bar_width / 2.0, plot_df[linevar2], color=self.mycolors[1], linestyle='--')
+        ax2.set_ylabel(ylabel2)
+
+        plt.tight_layout()
+        plt.show()
 
 
